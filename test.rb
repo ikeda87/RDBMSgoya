acd3c422e2c8bcbded5757280d5cb2ec7d6a8aac
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-07 15:11:26 +0000

    スペースなどを追加]

--- test.rb
+++ test.rb
@@ -8,9 +8,9 @@ server = WEBrick::HTTPServer.new({
   Signal.trap(signal){ server.shutdown }
 }
 server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicatename.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicatename.rb')
-server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.mount('/handovergoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'handovergoya.rb')
 server.mount('/lowqualitygoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'lowqualitygoya.rb')
 server.start
