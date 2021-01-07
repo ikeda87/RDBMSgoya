d7ba59e7ec78d0592f3092d49843555bf080a731
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-07 14:57:20 +0000

    test.rb追加

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '/',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicatename.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicatename.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/handovergoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'handovergoya.rb')
+server.mount('/lowqualitygoya.cgi', WEBrick::HTTPServlet::CGIHandler, 'lowqualitygoya.rb')
+server.start
